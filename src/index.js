20a393445397fe9baccd7c945843aad003ef04db
Julanick <julanick@gmail.com>
2019-11-22 02:08:43 +0300

    Add popout

--- src/index.js
+++ src/index.js
@@ -1,24 +1,49 @@
 import angular from 'angular';
+import ui from 'ui-bootstrap';
 
- var app = angular.module("AppModule", []);
- app.controller("TableTaskCntr", function ($scope) {
-     $scope.tasks = [
-         { name: "English", text: "Learn veb", flag: 1 },
-         { name: "Ticket", text: "Buy ticket", flag: 1 },
-         { name: "Contract", text: "Overwrite contract", flag: 1 },
-         { name: "Meet", text: "Meet partners", flag: 1 },
-         { name: "English", text: "Learn modal veb", flag: 1 },
-     ];
- });
+var app = angular.module("AppModule", [ui]);
+app.controller("TableTaskCntr", function($scope, $uibModal) {
+    $scope.tasks = [
+        { name: "English", text: "Learn veb", flag: 1 },
+        { name: "Ticket", text: "Buy ticket", flag: 1 },
+        { name: "Contract", text: "Overwrite contract", flag: 1 },
+        { name: "Meet", text: "Meet partners", flag: 1 },
+        { name: "English", text: "Learn modal veb", flag: 1 },
+    ];
 
- app.controller("SelectUrgencCntr", function ($scope) {
-     $scope.urgencys = ['low', 'moderate', 'high', 'extreme'];
-     $scope.selected = "low";
- });
+    $scope.open = function() {
+        var modalInstance = $uibModal.open({
+            templateUrl: "modalContent.html",
+            controller: "ModalContentCtrl",
+            size: '',
+        });
 
- app.controller("SelectStatusCntr", function ($scope) {
-     $scope.statuses = ['new', 'in progress', 'closed'];
-     $scope.selected = "new";
- });
+        modalInstance.result.then(function(response) {
+            $scope.result = `${response} button hitted`;
+        });
 
+    };
 
+});
+
+app.controller('ModalContentCtrl', function($scope, $uibModalInstance) {
+
+    $scope.ok = function() {
+        $uibModalInstance.close("Ok");
+    }
+
+    $scope.cancel = function() {
+        $uibModalInstance.dismiss();
+    }
+
+});
+
+app.controller("SelectUrgencCntr", function($scope) {
+    $scope.urgencys = ['low', 'moderate', 'high', 'extreme'];
+    $scope.selected = "low";
+});
+
+app.controller("SelectStatusCntr", function($scope) {
+    $scope.statuses = ['new', 'in progress', 'closed'];
+    $scope.selected = "new";
+});
